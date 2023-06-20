41857c2770786785719e260aca0a1ec41965c373
arcanstone <allan.henriques@mail.utoronto.ca>
2023-06-19 20:57:24 -0400

    Chapter 4.0
    I've added the stuff for chapter 4.
    This includes the:
    -go-on function
    -the go struct
    -the stop struct
    -And some imports

--- PotentialPotato.rkt
+++ PotentialPotato.rkt
@@ -1,5 +1,10 @@
 #lang racket
 
+;imports for chapter 4 error handling, specifically go-on
+(require (for-syntax syntax/parse))
+(require (for-syntax racket/base syntax/parse))
+
+
 ; env : Env
 ; var : symbol?
 ; body : any/c
@@ -113,6 +118,30 @@
        (λ (f)
          (λ (x)
            ((j f) ((k f) x)))))))
+
+
+; result : any/c
+(struct go (result) #:transparent)
+
+; expr : expression?
+; message : string?
+(struct stop (expr message) #:transparent)
+
+;
+(define-syntax (go-on stx)
+  (syntax-parse stx
+    [(go-on () result)
+     (syntax/loc stx
+       result)]
+    [(go-on ([pat0 e0] [pat e] ...) result)
+     (syntax/loc stx
+       (match e0
+         [(go pat0) (go-on ([pat e] ...) result)]
+         [(go v) (error 'go-on "Pattern did not match value ~v" v)]
+         [(stop expr msg) (stop expr msg)]))]))
+
+
+
 
 
 
